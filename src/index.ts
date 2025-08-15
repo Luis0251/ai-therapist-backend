bd3b122e262b54d28a1705bf3c239a1dce1879dd
Luis Quesada <luisdevcol@gmail.com>
2025-08-15 17:11:34 -0500

    add librarys and confirm log errors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,39 @@
 import {Request, Response} from 'express';
+import {serve} from 'inngest/express';
+import {inngest, functions} from './inngest/index';
+import {logger} from './utils/logger'
+import { log } from 'console';
 const express = require('express');
 
 const app = express();
 
 const PORT = 3001;
 
+app.use("/api/inngest", serve({client: inngest, functions}));
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.get('/api/chat', (req: Request, res: Response) => {
+  res.send('Hi, how can I help you?');
 });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+const startServer = async () => {
+  try{
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => {
+      logger.info(`Server is running on http://localhost:${PORT}`);
+      logger.info(`Inngest endpoint available at http://localhost:${PORT}/api/inngest`);
+    });
+  } catch (error) {
+    logger.error("Error starting server:", error);
+    process.exit(1);
+  }
+}
+
+startServer();
